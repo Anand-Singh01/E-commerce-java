14df3139b8c6c70fddfd6031c63f46141984c9a8
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-05-06 16:44:16 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './Customer/components/Navigation/Navigation';
+import HomePage from './Customer/pages/HomePage/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Navigation />
+      </div>
+      <div>
+        <HomePage />
+      </div>
+    </>
+
   );
 }
 
