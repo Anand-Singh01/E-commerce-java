ebe13ee1040a5efc3ef3c4c1aaa35ad3cd73c043
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-05-06 17:10:20 -0400

    Carousel implemented on home screen

---
+++ src/Customer/components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import { mainCarouselData } from './HomeCarouselData';
+
+export const MainCarousel = () => {
+    const items = mainCarouselData.map(({ image }) => {
+        return <img className='cursor-pointer' role='presentation' 
+        alt='' src={image}></img>
+    })
+    return (
+        <AliceCarousel
+            items={items}
+            disableButtonsControls
+            autoPlay
+            autoPlayInterval={5000}
+            infinite
+        />
+    )
+};
