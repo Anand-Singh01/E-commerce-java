60cb60bd0d2a83bfc8794736de342d07aa4eafa7
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-05-07 20:55:53 -0400

    Address card component completed.

--- src/Customer/components/AddressCard/AddressCard.jsx
+++ src/Customer/components/AddressCard/AddressCard.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const AddressCard = () => {
   return (
-    <div>AddressCard</div>
+    <div>
+        <div className='space-y-3'>
+            <p className='font-semibold'>Raam Kapoor</p>
+            <p>Mumbai, gokul</p>
+            <div className='space-y-1'>
+                <p className='font-semibold'>Phone Number</p>
+                <p>1111111111</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
